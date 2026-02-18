eda87e4741df2db7b8d672e223ab81c30eb69fdf
Raghavendra10365 <31158331+Raghavendra10365@users.noreply.github.com>
2026-02-18 11:43:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ document.getElementById("exploreBtn").addEventListener("click", function () {
         behavior: "smooth"
     });
 });
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    if (navLinks.style.display === "flex") {
+        navLinks.style.display = "none";
+    } else {
+        navLinks.style.display = "flex";
+    }
+});
